4fa19afc6bd376b1bc91c5e14871dc1dfb8f6df1
Ivan <104428966+ivan-er-dev@users.noreply.github.com>
2023-04-11 13:04:35 +0200

    Delete Back_SpotiApp directory

--- Back_SpotiApp/Back_SpotiApp/Models/Genero.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Back_SpotiApp.Models
-{
-    public class Genero
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<Cancion> canciones { get; set; }
-    }
-}
